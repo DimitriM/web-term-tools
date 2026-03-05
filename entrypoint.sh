c9d85136828c5949de84fbf1fcc75214d2b74b24
Dimitri Marechal <mdimitri@mddev.be>
2026-03-05 11:08:44 +0100

    Added prefix option

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,8 @@ if [ -n "$TTYD_USERNAME" ] && [ -n "$TTYD_PASSWORD" ]; then
     TTYD_ARGS="-c ${TTYD_USERNAME}:${TTYD_PASSWORD} $TTYD_ARGS"
 fi
 
+if [ -n "$TTYD_PREFIX" ]; then
+    TTYD_ARGS="-b ${TTYD_PREFIX} $TTYD_ARGS"
+fi
+
 exec ttyd $TTYD_ARGS /bin/sh -i -l
